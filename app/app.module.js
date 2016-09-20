79e951bec505248fb4b1a8017dd92f9a998428b3
piro <kemalpiro@gmail.com>
2016-09-20 19:00:20 +0200

    fix access-control-allow-origin policy problem

--- app/app.module.js
+++ app/app.module.js
@@ -2,4 +2,8 @@
     angular.module('RentalSearchApp', [
         'ui.router', 'ngMaterial', 'ngMdIcons', 'rental-services'
     ])
+    angular.module('RentalSearchApp')
+        .config(function($httpProvider){
+            $httpProvider.defaults.headers.common = ['X-Requested-With', 'Content-Type'];
+        });
 })();
