40f76610d58a023bdbc776813be36e8f7461897e
burnpiro <kemalpiro@gmail.com>
2019-09-03 22:19:08 +0200

    Remove old code

--- app/app.module.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    angular.module('RentalSearchApp', [
-        'ui.router', 'ngMaterial', 'ngMdIcons', 'rental-services'
-    ])
-    angular.module('RentalSearchApp')
-        .config(function($httpProvider){
-            $httpProvider.defaults.headers.common = ['X-Requested-With', 'Content-Type'];
-        });
-})();
--- app/background.module.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    angular.module('RentalBackgroundApp', [
-        'ui.router', 'rental-services'
-    ])
-})();
